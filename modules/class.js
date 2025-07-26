da891335eeb4eeedfcd620d217cafdf7b43a5323
ariyu-indie <ariyusolaris@gmail.com>
2025-07-26 19:57:10 +0800

    Updated class.js
    this module isn't really a module, it's just a seperate for the trissaeths in my game!

--- modules/class.js
+++ modules/class.js
@@ -1,4 +1,4 @@
-import { random, generatePhonemes, generateGroups, generateRand as gr } from "./basics.js"
+import { random, generatePhonemes, generateGroups, generateRand as gr, generateText as gt} from "./basics.js"
 
 const reactions = [
     "i barely noticed a touch on my {area}.",
@@ -34,7 +34,28 @@ const reactions = [
     "i think my {area} just gave out..."
 ]
 
-function check(a, b){
+const reactionTALK = [
+    "I talked to {other}, it was boring.",
+    "I tried talking to {other}, but we didn't have much to say.",
+    "I spoke to {other}, but it felt awkward.",
+    "I had a short chat with {other}. It was fine.",
+    "I talked with {other}. Not bad, not great.",
+    "I had a decent conversation with {other}.",
+    "I enjoyed talking with {other}. It felt natural.",
+    "Me and {other} had a really good talk. I felt heard.",
+    "Talking to {other} was exciting—we laughed and shared stories.",
+    "Me and {other} talked together, it was AWESOME!"
+]
+export function Getmood(mood){
+    if (mood<1.0){return "😡"}
+    if (mood<2.0){return "😭"}
+    if (mood<3.0){return "😢"}
+    if (mood<4.0){return "😔"}
+    if (mood<5.0){return "😐"}
+    if (mood<8.0){return "😊"}
+    return "😁"
+}
+export function check(a, b){
     const truth = {
         "MALE": "FEMALE",
         "FEMALE": "MALE"
@@ -44,22 +65,24 @@ function check(a, b){
     }
 }
 
-export default class trissaeths{
+export default function main(){
+    return "ver@0.1"
+}
+
+export class trissaeths{
     constructor(name, age, str, int, agi) {
         this.name = name
         this.state = "alive"
-        this.location = ""
+        this.mood = 5.0
+        this.location
+        this.birthday = 0
         this.memory = [
-            {
-                "name": "birthday",
-                "content": "my birthday is",
-                "strength": 28000
-            }
         ]
         this.parent = {
-            "mother": null,
-            "father": null
+            "mother": "god",
+            "father": "god"
         }
+        this.viruses = []
         this.body = {
             "head": {
                 "health": 100,
@@ -91,7 +114,7 @@ export default class trissaeths{
                 "tolerance": 50+gr(0, 60),
                 "intact": true
             },
-            "hips": {
+            "hip": {
                 "health": 100,
                 "tolerance": 40+gr(0, 30),
                 "intact": true
@@ -113,19 +136,29 @@ export default class trissaeths{
             }
         }
         this.ancestors = {
-            "greatGrandma": "god",
-            "greatGrandpa": "god",
-            "grandma": "god",
-            "grandpa": "god",
-            "parentA": "god",
-            "parentB": "god"
+            "grandma": [
+            ],
+            "grandpa": [
+            ],
+            "mother": "god",
+            "father": "god"
         }
+        this.children = []
+        this.impow = gr(0.1, 5) //means immunity power
+        this.gender = random(["MALE", "FEMALE"])
+        this.willpower = gr(0, 1)
         this.fertile = false
         this.fertileAge = Math.round(gr(13, 18))
+        this.fertileEx = Math.round(gr(this.fertileAge, 80))
         this.language = ""
-        this.gender = random(["MALE", "FEMALE"])
+        this.groups = {
+            "in": false,
+            "groups": []
+        }
+        this.color = ""
         this.age = age
         this.heatRes = 30
+        this.relationship = []
         this.traits = {
             "str": str,
             "int": int,
@@ -135,66 +168,25 @@ export default class trissaeths{
     checkself(){
         if (this.state === "alive"){
             if (this.body["head"]["health"] <= 0){
+                console.log(this.name + " just died!")
                 this.state = "dead"
             }
         }
         if (this.age >= this.fertileAge){
             this.fertile = true
         }
-    }
-    reprod(other){
-        if (this.fertile === true && other.fertile === true){
-        if (check(this.gender, other.gender)){
-            if (this.body["genitalia"]["health"] >= 50 && other.body["genitalia"]["health"] >= 50){
-                let n = new trissaeths(other.name + this.name, 0, 0, 0, 0)
-                let str = parseFloat((this.traits["str"] + other.traits["str"]).toFixed(2))
-                let int = parseFloat((this.traits["int"] + other.traits["int"]).toFixed(2))
-                let agi = parseFloat((this.traits["agi"] + other.traits["agi"]).toFixed(2))
-                let parentA = this.name
-                let parentB = other.name
-                let language = random([this.language, other.language])
-                n.traits["str"] = str - parseFloat((Math.random()*str+1).toFixed(2))
-                n.traits["int"] = int - parseFloat((Math.random()*int+1).toFixed(2))
-                n.traits["agi"] = agi - parseFloat((Math.random()*agi+1).toFixed(2))
-                n.language = language
-                n.ancestors = []
-                n.ancestors.push({
-                    "greatGrandma": [
-                        `${parentA}'s mother ${this.ancestors["grandma"]}`,
-                        `${parentB}'s mother ${other.ancestors["grandma"]}`
-                    ],
-                    "greatGrandpa": [
-                        `${parentA}'s mother ${this.ancestors["grandpa"]}`,
-                        `${parentB}'s mother ${other.ancestors["grandpa"]}`
-                    ],
-                    "grandma": [
-                        `${parentA}'s mother ${this.parent["mother"]}`,
-                        `${parentB}'s mother ${other.parent["mother"]}`
-                    ],
-                    "grandpa": [
-                        `${parentA}'s father ${this.parent["father"]}`,
-                        `${parentB}'s father ${other.parent["father"]}`
-                    ],
-                    "parentA": parentA, 
-                    "parentB": parentB
-                })
-                if (this.gender === "MALE"){
-                    n.parent["father"] = parentA
-                } else {
-                    n.parent["mother"] = parentA
-                }
-                if (other.gender === "MALE"){
-                    n.parent["father"] = parentB
-                } else {
-                    n.parent["mother"] = parentB
-                }
-                return n
-            } else {
-                return
-            }
-        } else {
-            return
+        if (this.age >= this.fertileEx){
+            this.fertile = false
         }
+        if (this.mood > 10){
+            this.mood = 10
+        } else if (this.mood < 0){
+            this.mood = 0
+        }
+        if (this.groups["in"] = true){
+            if (this.groups["groups"].length <= 0){
+                this.groups["in"] = false
+            }
         }
     }
     damage(area, num){
@@ -205,6 +197,7 @@ export default class trissaeths{
                     this.body[area]["health"] = 0
                 }
             }
+            this.mood -= 1.0
             let reaction = Math.floor((num / this.body[area]["tolerance"])*reactions.length)
             if (reaction > +reactions.length-1){
                 reaction = +reactions.length-1
@@ -235,22 +228,183 @@ export default class trissaeths{
             }
         }
     }
+    know(other){
+        for (let i in this.relationship){
+            if (other in this.relationship[i]["target"]){
+                return true
+            }
+        }
+        return false
+    }
+    talk(other){
+        if (this.location.id === other.location.id){
+            let a = gr(0, 10)
+            let b = a / reactionTALK.length
+            let intensity = Math.floor(b*9)
+            let res = reactionTALK[intensity]
+            res = res.replace("{other}", this.name)
+            if (this.mood < 2.0){
+                let act = random(["attack", "no"])
+                if (act === "attack"){
+                    other.damage("head", gr(0, 20))
+                }
+                other.mood -= (gr(-2, 2)-other.willpower)
+                if (other.know(this)){
+                    other.memory.push({
+                        "name": "i got yelled at.",
+                        "content": `${this.name} yelled at me...`,
+                        "subject": this.name,
+                        "strength": gr(10, 20)
+                    })
+                } else {
+                    other.memory.push({
+                        "name": "i got yelled at.",
+                        "content": `someone yelled at me...`,
+                        "subject": this.name,
+                        "strength": gr(10, 20)
+                    })
+                }
+                if (this.know(other)){
+                    this.memory.push({
+                        "name": `i yelled at ${other.name}`,
+                        "content": `i yelled at ${other.name} for talking
+                        to me.`,
+                        "subject": other.name,
+                        "strength": gr(20, 30)
+                    })
+                } else {
+                    this.memory.push({
+                        "name": "i yelled at someone",
+                        "content": `i yelled at someone I don't know.`,
+                        "subject": other.name,
+                        "strength": gr(10, 20)
+                    })
+                }
+            } else {
+            if (intensity > 5){
+                this.mood += 0.8
+            }
+            if (intensity < 5){
+                this.mood -= 0.1
+            }
+            other.memory.push(
+                {
+                    "name": `talk with ${this.name}`,
+                    "content": `${res}`,
+                    "strength": intensity
+                }
+            )
+            }
+        }
+    }
 }   
 
-export class language{
-    constructor(){
-        this.phonemes = generatePhonemes()
+const virusesR = [
+    "this virus doesn't do anything to the {area}",
+    "this virus causes minor damages to the {area}",
+    "this virus can cause mild damages to the {area}",
+    "this virus always cause {area} damage",
+    "this virus causes extreme damage to the {area}",
+    "this virus can destroy the infected's {area}"
+]
+
+const damage = [
+    "this virus may cause little to no harm",
+    "this virus cause mild to minor harm",
+    "this virus causes mild harm",
+    "this virus causes harm",
+    "this virus causes extreme harm",
+    "this virus may cause death"
+]
+
+function generateHarm(a){
+    let reac = Math.floor((a / 10) * (damage.length))
+    console.log(reac)
+    let k = damage[reac]
+    return k
+}
+
+function generateViDesc(a){
+    let k = ""
+    let descRes = 0
+    let ko
+    let p 
+    let ou
+    let n = 1
+    let yoc = 8
+    for (let i in a){
+        ko = a[i]
+        p = 10
+        ou = ko / p
+        descRes = Math.floor(ou * virusesR.length)
+        if (descRes <= 0){
+            descRes = 0
+        }
+        if (n < yoc){
+            k += virusesR[descRes] + ", "
+        } else if (n >= yoc){
+            k += virusesR[descRes] + "."
+        }
+        k = k.replace("{area}", i)
+        n++
+    }
+    return k
+}
+
+export class virus{
+    constructor (){
+        this.name = "unknown",
+        this.attacks = {
+            "head": gr(-5, 10),
+            "neck": gr(-5, 10),
+            "left_arm": gr(-5, 10),
+            "right_arm": gr(-5, 10),
+            "hip": gr(-5, 10),
+            "genitalia": gr(-5, 10),
+            "left_leg": gr(-5, 10),
+            "right_leg": gr(-5, 10),
+        }
+        let tempo = 0
+        let n = 0
+        for (let o in this.attacks){
+            tempo += this.attacks[o]
+            if (this.attacks[o] < 0){
+                this.attacks[o] = 0
+            }
+            n++
+        }
+        this.severity = tempo / n
+        this.damage = generateHarm(this.severity)
+        this.desc = generateViDesc(this.attacks)
+        this.virusLife = gr(0, 100)
+        this.behavior = {
+            "AIRBORNE": random([true, false]),
+        }
     }
 }
 
-export class country{
-    constructor(name, lname, origin){
+export class location{
+    constructor(){
+        this.id = Math.round(Math.random()*999999999)
+        this.name = "unknown"
+        this.resources = []
+        this.connections = []
+        this.visited = false
+    }
+}
+
+export class language{
+    constructor(alphabet){
+        this.phonemes = generatePhonemes(alphabet)
+        this.groups = generateGroups(this.phonemes)
+        this.name = gt(this.groups)
+    }
+}
+
+export class group{
+    constructor(name){
         this.name = name
         this.age = 0
-        this.language = {
-            "name": lname,
-            "emerged": 0,
-            "origin": origin
-        }
+        this.members = []
     }
 }
